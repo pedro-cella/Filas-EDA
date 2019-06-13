652fa0145f87b5b79e6d828e933d7307ba233a40
pedro-cella <49076513+pedro-cella@users.noreply.github.com>
2019-06-13 14:04:04 -0300

    fila_redimensionamento.h criada

---
+++ fila_redimensionamento.h
@@ -0,0 +1,10 @@
+#ifndef fila_redimensionamento_h
+#define fila_redimensionamento_h
+
+void cria_fila();
+int enfileira(int c);
+int desenfileira(int *c);
+int fila_vazia();
+int fila_cheia();
+
+#endif
