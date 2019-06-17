560baba9ddcf52b65341012d90f2938d02bdc2be
pedro-cella <49076513+pedro-cella@users.noreply.github.com>
2019-06-17 10:48:40 -0300

    Atualizacao da main

--- main.c
+++ main.c
@@ -1,45 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "fila_simples.h"
+#include "fila_redimensionamento.h"
 
 int main()
 {
-    int opcao, N, numero;
-    do
-    {
-        printf("MENU\n");
-        printf("Escolha a opca desejada:\n");
-        printf("1- Inserir n elementos na fila.\n2- Remover n elementos na fila.\n3- Imprimir a fila.\n4- Sair.");
-        scanf("%d", &opcao);
-        system("clear");
+  int elem, ret;
+  
+  if(fila_vazia()) {
+    cria_fila();
+  }
+    
+  int opcao, N, numero, i;
+  do {
+    printf("MENU\n");
+    printf("Escolha a opca desejada:\n");
+    printf("1- Inserir n elementos na fila.\n2- Remover n elementos na fila.\n3- Imprimir a fila.\n4- Sair.\nOpção: ");
+    scanf("%d", &opcao);
+    // system("clear");
+    switch(opcao)
+      {
+      case 1:
+	printf("Digite quantos elementos voce quer inserir: ");
+	scanf("%d", &N);
+	ret = 1;
+	for(i = 0; i < N && ret; i++)
+	  {
+	    /* if(fila_cheia())//\**ERRO**  */
+	    /* { */
+	    /*     //printf("Não foi possivel inserir %d numeros pois a fila encheu.\n",N-i); */
+	    /* 	redimensiona();//<-------ERRO! */
+	    /* 	break; */
+	    /* } */
+	    printf("Inserido o %dº elemento:\n",i+1);
+	    scanf("%d", &numero);
+	    if(numero >= 0 && numero <= 999)
+	      {
+		ret = enfileira( numero );
+		if ( ret == -1 ) {
+		  printf( "Erro de alocacao\n" );
+		}
+		else {
+		  if ( ret == 0 ) {
+		    printf( "Fila cheia\n" );
+		  }
+		}
+	      }
+	    else
+	      {
+		i--;
+		printf("O numero %d nao foi inserido, pois nao e um numero valido\n", numero);
+	      }
+	  }
+	break;
 
-	switch(opcao)
-	{
-            case 1:
-		    printf("Digite quantos elementos voce quer inserir: ");
-		    scanf("%d\n", &N);
-		    for(i = 0; i < N; i++)
-		    {
-	                scanf("%d", &numero);
-                        if(numero >= 0 && numero <= 999)
-			{
+      case 2:
+	printf("Digite quantos elementos voce quer remover: ");
+	scanf("%d", &N);
+	for(i = 0; i < N && ret; i++)
+	  {
+	    ret = desenfileira( &elem );
+	    if(ret == 0) {
+	      printf("Não foi possivel remover %d numeros pois a fila está vazia.\n",N-i);
+	      break;
+	    }
+	    else {
+		printf("Removido o numero: %d.\n",elem);
+	    }
+	  }
+	break;
+		
+      case 3:
+	imprimir_fila();
+	break;
+      }
 
-			}
-			    else
-			    {
-                                printf("O numero %d nao foi inserido, pois nao e um numero valido", numero);
-			    }
-		    }
-                    break;
-
-            case 2:
-		    printf("Digite quantos elementos voce deseja remover: ");
-		    scanf("%d\n", &N);
-		    break;
-	    case 3:
-		    break;
-	}
-    }while(opcao != 4);
-
-
+  }while(opcao != 4);
 }
