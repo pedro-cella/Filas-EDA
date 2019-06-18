80256b6d43922d73eb4d00620405368e8e955cac
pedro-cella <49076513+pedro-cella@users.noreply.github.com>
2019-06-18 12:45:36 -0300

    Retirada do imprimir

--- fila_lista.c
+++ fila_lista.c
@@ -5,6 +5,32 @@ struct elem
 {
     int dado;
     struct elem *prox;
-};
+}celula;
 
+celula *fi;
+fi = malloc(sizeof(celula));
+fi -> prox = fi
 
+int enfileira(int y, celula *fi)
+{
+    celula *nova;
+    nova = malloc(sizeof(celula));
+    nova -> prox = fi -> prox;
+    fi -> prox = nova;
+    fi -> conteudo = y;
+    return nova;
+}
+
+int desenfileira(lista *fi)
+{
+    celula *p;
+    p = fi -> prox;
+    int x = p -> conteudo;
+    fi -> prox = p -> prox;
+    free(p);
+    return x;
+}
+void imprimir_fila(lista *le)
+{
+
+}
