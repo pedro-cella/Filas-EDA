bcb627e2f74545691523f824f6c5f277baddfbd6
Pedro Cella <49076513+pedro-cella@users.noreply.github.com>
2019-06-20 12:03:22 -0300

    Atualizacao fila_lista.h

---
+++ fila_lista.h
@@ -0,0 +1,12 @@
+#ifndef FILA_LISTA_H
+#define FILA_LISTA_H
+
+void cria_fila();
+int enfileira(int y);
+int desenfileira();
+int fila_vazia();
+int fila_cheia();
+void imprimir_fila();
+void freeFila();
+
+#endif
