04361fea3a9a9f35541a6bbeb12123a900cbcdb9
Pedro Cella <49076513+pedro-cella@users.noreply.github.com>
2019-06-20 12:03:03 -0300

    Atualizacao fila_lista.c

--- fila_lista.c
+++ fila_lista.c
@@ -1,40 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include "fila_lista.h"
 
 struct elem
 {
-    int dado;
-    struct elem *prox;
-}celula;
+	int dado;
+	struct elem *prox;
+};
 
-celula *lista;
+typedef struct elem lista;
+
+static lista *ListaCabeca;
+
 
 void cria_fila()
 {
-    lista = malloc(sizeof(celula));
-    lista -> prox = lista;
+	ListaCabeca = malloc(sizeof(lista));
+	ListaCabeca->prox = NULL;
+   
 }
 
-int enfileira(int y, celula *lista)
+int enfileira(int y)
 {
-    celula *nova;
-    nova = malloc(sizeof(celula));
-    nova -> prox = lista -> prox;
-    lista -> prox = nova;
-    lista -> conteudo = y;
-    return nova;
+   lista *nova;
+   nova = malloc (sizeof (lista));
+   nova->prox = ListaCabeca->prox; 
+   nova->dado = y;
+   ListaCabeca->prox = nova;   
+   return 1;
 }
 
-int desenfileira(lista *lista)
+int desenfileira()
 {
-    celula *p;
-    p = lista -> prox;
-    int x = p -> conteudo;
-    lista -> prox = p -> prox;
-    free(p);
-    return x;
+   lista *retirar;
+   retirar = ListaCabeca->prox;  // o primeiro da fila
+   int x = retirar->dado;
+   ListaCabeca->prox = retirar->prox;
+   free (retirar);
+   return x; 
 }
-void imprimir_fila()
-{
 
+
+int fila_vazia()
+{
+    return ListaCabeca->prox == NULL;
+
+}
+
+int fila_cheia()
+{
+   return 0;
+}
+
+void imprimir_fila(){
+     printf("+");
+     lista *impressao;
+    for (impressao = ListaCabeca -> prox; impressao != NULL; impressao = impressao->prox){
+    	printf("-----+");
+    }
+    printf("\n");
+    printf("|");
+
+   	impressao = ListaCabeca->prox;
+   
+    while(impressao != NULL){
+
+    	printf(" %03d |", impressao->dado);
+    	impressao = impressao->prox;
+    }
+
+    printf("\n");
+    printf("+");
+
+    for (impressao = ListaCabeca -> prox; impressao != NULL; impressao = impressao->prox){
+    	printf("-----+");
+    }
+
+    printf("\n");
+}
+
+void freeFila(){
+    free(ListaCabeca);
 }
